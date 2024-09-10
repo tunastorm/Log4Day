5b94a2a10e9a976b9e4df423e97a4d57cf2a3c49
tunastorm <ycw921@naver.com>
2024-09-10 21:50:06 +0900

    Initial Commit

---
+++ Log4Day/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Log4Day
+//
+//  Created by 유철원 on 9/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Log4Day/Log4DayApp.swift
@@ -0,0 +1,17 @@
+//
+//  Log4DayApp.swift
+//  Log4Day
+//
+//  Created by 유철원 on 9/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Log4DayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
